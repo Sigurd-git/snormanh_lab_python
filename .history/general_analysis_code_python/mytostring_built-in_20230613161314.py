82bb01b72e8208c17fc898f576fa502054722cb1
Kcriss <jiafujun123@gmail.com>
2023-06-21 00:50:39 -0400

    TODO: DataHash

---
+++ .history/general_analysis_code_python/mytostring_built-in_20230613161314.py
@@ -0,0 +1,28 @@
+def mytostring(x, delimiter='-', hashlen=float('NaN')):
+    """
+    Built-in version
+    -------------
+    Relatively general function for converting an input to a string that represents its content. Used by struct2string.
+
+    Example:
+   
+    print(mytostring([2.33, 3, 4, 1, 'test']))
+
+    """
+    if x is None:
+        s = 'NULL'
+    elif isinstance(x, str):
+        s = x
+    elif (isinstance(x, (int, float)) or isinstance(x, bool)) and isinstance(x, (int, float, str)):
+        s = '-'.join(map(str, x))
+        if not math.isnan(hashlen) and len(s) > hashlen:
+            s = 'DataHash(x)'  # Replace this line with your DataHash function
+    elif isinstance(x, list):
+        s = delimiter.join(mytostring(i) for i in x)
+    else:
+        raise ValueError('No valid type')
+
+    return s
+
+
+if __name__ == '__main__':
---
+++ .history/general_analysis_code_python/mytostring_built-in_20230613220216.py
@@ -0,0 +1,32 @@
+import DataHash
+import math
+
+def mytostring(x, delimiter='-', hashlen=float('NaN')):
+    """
+    Built-in version
+    -------------
+    Relatively general function for converting an input to a string that represents its content. Used by struct2string.
+
+    Example:
+   
+    print(mytostring([2.33, 3, 4, 1, 'test']))
+
+    """
+    if x is None:
+        s = 'NULL'
+    elif isinstance(x, str):
+        s = x
+    elif (isinstance(x, (int, float)) or isinstance(x, bool)) and isinstance(x, (int, float, str)):
+        s = '-'.join(map(str, x))
+        if not math.isnan(hashlen) and len(s) > hashlen:
+            s = DataHash(x)
+    elif isinstance(x, list):
+        s = delimiter.join(mytostring(i) for i in x)
+    else:
+        raise ValueError('No valid type')
+
+    return s
+
+
+if __name__ == '__main__':
+    print(mytostring([2.33, 3, 4, 1, 'test']))
